8f9dd8de571a24988f17080abfa456f1a227b201
PokoenkoS <pokoenkosveta@gmail.com>
2022-12-20 16:05:32 +0200

    add a task 6

---
+++ js/task-06.js
@@ -0,0 +1,21 @@
+const inputEl = document.querySelector(`#validation-input`);
+
+const lengthEl = inputEl.getAttribute("data-length");
+console.log(lengthEl);
+
+inputEl.addEventListener(`blur`, onInputBlur);
+
+function onInputBlur(event) {
+    if (Number(event.currentTarget.value.length) === Number(lengthEl)) {
+        inputEl.classList.add(`valid`);
+            
+    } else {
+      
+        inputEl.classList.add(`invalid`);
+
+ 
+}
+console.log(event.currentTarget.value);
+
+};
+ 
