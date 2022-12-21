48397e9f596deebb98beb2b513d47b9d3f35a8b8
PokoenkoS <pokoenkosveta@gmail.com>
2022-12-21 09:08:07 +0200

    add a task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,16 +1,10 @@
 const inputEl = document.querySelector(`#font-size-control`);
 const nameLabel = document.querySelector(`#text`)
-// nameLabel.classList.add(`font-size`);
-console.log(nameLabel);
+
 inputEl.addEventListener(`input`, onInputBlur);
 
-function onInputBlur(event) {
-    // document.getElementById("text").style.fontSize = "event.currentTarget.value"
-//     document.getElementById(`text`).font-size = "event.currentTarget.value";
-//     document.body.span.style.font-size = 'event.currentTarget.value';
-// nameLabel.textContent = event.currentTarget.value;
-nameLabel.classList.add(`font-size = "${event.currentTarget.value}"`);
-nameLabel.style.
-console.log(nameLabel);
-console.log(event);
+function onInputBlur() {
+
+nameLabel.style.fontSize = inputEl.value + 'px';
+
 }
