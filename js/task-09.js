84a22f5060293d6d11241260e0fd44b2861842fd
PokoenkoS <pokoenkosveta@gmail.com>
2022-12-21 16:49:37 +0200

    add a task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeBtn = document.querySelector(`.change-color`);
+const spanEl = document.querySelector(`.color`)
+
+changeBtn.addEventListener(`click`, onchangeColor);
+
+function onchangeColor() {
+spanEl.textContent = document.body.style.background = getRandomHexColor();
+
+}
